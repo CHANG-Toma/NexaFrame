abcf4940a8dde9ff0959d443f62b57a4b1ffddaa
Toma Chang <toma11chang@gmail.com>
2024-03-02 17:24:57 +0100

    feat : models created

---
+++ app/Models/comments.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\DB;
+
+Class comments extends DB{
+    private $id;
+    private $articleId;
+    private $commentResponseId;
+    private $userId;
+    private $content;
+    private $createdAt;
+    private $valid;
+    private $validateAt;
+    private $validatorId;
+    private $updatedAt;
+
+    public function __construct($id, $articleId, $commentResponseId, $userId, $content, $createdAt, $valid, $validateAt, $validatorId, $updatedAt) {
+        $this->id = $id;
+        $this->articleId = $articleId;
+        $this->commentResponseId = $commentResponseId;
+        $this->userId = $userId;
+        $this->content = $content;
+        $this->createdAt = $createdAt;
+        $this->valid = $valid;
+        $this->validateAt = $validateAt;
+        $this->validatorId = $validatorId;
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getArticleId() {
+        return $this->articleId;
+    }
+
+    public function setArticleId($articleId) {
+        $this->articleId = $articleId;
+    }
+
+    public function getCommentResponseId() {
+        return $this->commentResponseId;
+    }
+
+    public function setCommentResponseId($commentResponseId) {
+        $this->commentResponseId = $commentResponseId;
+    }
+
+    public function getUserId() {
+        return $this->userId;
+    }
+
+    public function setUserId($userId) {
+        $this->userId = $userId;
+    }
+
+    public function getContent() {
+        return $this->content;
+    }
+
+    public function setContent($content) {
+        $this->content = $content;
+    }
+
+    public function getCreatedAt() {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt($createdAt) {
+        $this->createdAt = $createdAt;
+    }
+
+    public function isValid() {
+        return $this->valid;
+    }
+
+    public function setValid($valid) {
+        $this->valid = $valid;
+    }
+
+    public function getValidateAt() {
+        return $this->validateAt;
+    }
+
+    public function setValidateAt($validateAt) {
+        $this->validateAt = $validateAt;
+    }
+
+    public function getValidatorId() {
+        return $this->validatorId;
+    }
+
+    public function setValidatorId($validatorId) {
+        $this->validatorId = $validatorId;
+    }
+
+    public function getUpdatedAt() {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt($updatedAt) {
+        $this->updatedAt = $updatedAt;
+    }
+}
--- app/views/front-office/main/installer_configAdmin.php
+++ app/views/front-office/main/installer_configAdmin.php
@@ -1,30 +1,41 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/dist/css/main.css" />
-    <title>Installer - Admin Account</title>   
+    <title>Installer - Admin Account</title>
 </head>
+
 <body>
-    <div class="form-container">
+    <div class="installer-container">
+        <h2>Create Admin Account</h2>
         <?= $message ?>
-        <h1>Create Admin Account</h1>
         <form action="/admin/create" method="POST">
             <div class="form-group">
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" required>
+                <label for="domainName">Nom de domaine :</label>
+                <input type="text" id="domainName" name="domain-name" required>
+            </div>
+            <div class="form-group">
+                <label for="login">Login:</label>
+                <input type="text" id="login" name="login" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
             </div>
             <div class="form-group">
                 <label for="password">Password:</label>
                 <input type="password" id="password" name="password" required>
             </div>
             <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
+                <label for="password">Repeat password:</label>
+                <input type="password" id="repeat-password" name="repeat-password" required>
             </div>
-            <button type="submit">Create Account</button>
+            <button class="Button Primary" type="submit">Create Account</button>
         </form>
     </div>
 </body>
+
 </html>
