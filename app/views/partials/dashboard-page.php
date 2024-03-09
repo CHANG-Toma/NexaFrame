8f2a37d5ac86585212d9b8168d487f5454e92064
CHANG-Toma <toma11chang@gmail.com>
2024-03-09 15:49:05 +0100

    feat : responsive PageBuilder -> with grapesjs

--- app/views/partials/dashboard-page.php
+++ app/views/partials/dashboard-page.php
@@ -1,17 +1,18 @@
 <div class="panel__top">
     <div class="panel__basic-actions"></div>
+    <div class="panel__devices"></div>
     <div class="panel__switcher"></div>
 </div>
 <div class="editor-row">
-  <div class="editor-canvas">
-    <div id="gjs">
-      <h1>Dashboard</h1>
+    <div class="editor-canvas">
+        <div id="gjs" style="height:0px; overflow:hidden;">
+            <h1>Dashboard</h1>
+        </div>
     </div>
-  </div>
-  <div class="panel__right">
-    <div class="layers-container"></div>
-    <div class="styles-container"></div>
-  </div>
+    <div class="panel__right">
+        <div class="layers-container"></div>
+        <div class="styles-container"></div>
+    </div>
 </div>
 
 <div id="blocks"></div>
--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -1,71 +1,36 @@
 import "grapesjs/dist/css/grapes.min.css";
-import grapesjs from "grapesjs";
+import grapesjs from "grapesjs/dist/grapes.min.js";
 
 // Créez une nouvelle instance de GrapesJS avec la configuration souhaitée
 const editor = grapesjs.init({
-  // Indiquez où initialiser l'éditeur. Vous pouvez également passer un élément HTMLElement
+  showOffsets: 1,
+  noticeOnUnload: 0,
   container: "#gjs",
-  // Obtenez le contenu du canvas directement à partir de l'élément
-  // En alternative, nous pourrions utiliser: `components: '<h1>Hello World Component!</h1>'`,
+  height: "100%",
   fromElement: true,
-  // Taille de l'éditeur
-  height: "300px",
-  width: "auto",
-  // Désactivez le gestionnaire de stockage pour le moment
-  storageManager: false,
+  storageManager: { autoload: 0 },
 
-  // Blocs d'ajout
-  blockManager: {
-    appendTo: "#blocks",
-    blocks: [
-      {
-        id: "section", // l'ID est obligatoire
-        label: "<b>Section</b>", // Vous pouvez utiliser du HTML/SVG à l'intérieur des libellés
-        attributes: { class: "gjs-block-section" },
-        content: `<section>
-                            <h1>Ceci est un titre simple</h1>
-                            <div>Ceci est juste un texte Lorem : Lorem ipsum dolor sit amet</div>
-                        </section>`,
-      },
-      {
-        id: "text",
-        label: "Texte",
-        content: '<div data-gjs-type="text">Insérez votre texte ici</div>',
-      },
-      {
-        id: "image",
-        label: "Image",
-        // Sélectionnez le composant une fois qu'il est déposé
-        select: true,
-        // Vous pouvez passer des composants sous forme de JSON au lieu d'une simple chaîne HTML,
-        // dans ce cas, nous utilisons également un type de composant défini `image`
-        content: { type: "image" },
-        // Cela déclenche l'événement `active` sur les composants déposés et l'`image`
-        // réagit en ouvrant l'AssetManager
-        activate: true,
-      },
-    ],
+  // Ajout de la gestion des plugins pour une meilleure extensibilité
+  plugins: ["gjs-blocks-basic"],
+  pluginsOpts: {
+    "gjs-blocks-basic": {
+      /* options */
+    },
   },
 
-  layerManager: {
-    appendTo: ".layers-container",
-  },
-  // We define a default panel as a sidebar to contain layers
+  // Ajout de la gestion des composants pour une meilleure extensibilité
   panels: {
     defaults: [
       {
         id: "layers",
         el: ".panel__right",
-        // Make the panel resizable
         resizable: {
           maxDim: 350,
           minDim: 200,
-          tc: 0, // Top handler
-          cl: 1, // Left handler
-          cr: 0, // Right handler
-          bc: 0, // Bottom handler
-          // Being a flex child we need to change `flex-basis` property
-          // instead of the `width` (default)
+          tc: 0,
+          cl: 1,
+          cr: 0,
+          bc: 0,
           keyWidth: "flex-basis",
         },
       },
@@ -78,7 +43,6 @@ const editor = grapesjs.init({
             active: true,
             label: "Layers",
             command: "show-layers",
-            // Once activated disable the possibility to turn it off
             togglable: false,
           },
           {
@@ -90,61 +54,168 @@ const editor = grapesjs.init({
           },
         ],
       },
+      {
+        id: "panel-devices",
+        el: ".panel__devices",
+        buttons: [
+          {
+            id: "device-desktop",
+            label: "D",
+            command: "set-device-desktop",
+            active: true,
+            togglable: false,
+          },
+          {
+            id: "device-tablet",
+            label: "T",
+            command: "set-device-tablet",
+            togglable: false,
+          },
+          {
+            id: "device-mobile",
+            label: "M",
+            command: "set-device-mobile",
+            togglable: false,
+          },
+        ],
+      },
+    ],
+  },
+
+  deviceManager: {
+    devices: [
+      {
+        name: "Desktop",
+        width: "",
+        widthMedia: "1024px",
+      },
+      {
+        name: "Tablet",
+        width: "768px",
+        widthMedia: "992px",
+      },
+      {
+        name: "Mobile",
+        width: "320px",
+        widthMedia: "576px",
+      },
     ],
   },
 
   selectorManager: {
     appendTo: ".styles-container",
   },
+
+  layerManager: {
+    appendTo: ".layers-container",
+  },
+
   styleManager: {
     appendTo: ".styles-container",
     sectors: [
       {
+        name: "General",
+        open: false,
+        buildProps: [
+          "float",
+          "display",
+          "position",
+          "top",
+          "right",
+          "left",
+          "bottom",
+        ],
+      },
+      {
+        name: "Flex",
+        open: false,
+        buildProps: [
+          "flex-direction",
+          "flex-wrap",
+          "justify-content",
+          "align-items",
+          "align-content",
+          "order",
+          "flex-basis",
+          "flex-grow",
+          "flex-shrink",
+          "align-self",
+        ],
+      },
+      {
         name: "Dimension",
         open: false,
-        // Use built-in properties
-        buildProps: ["width", "min-height", "padding"],
-        // Use `properties` to define/override single property
-        properties: [
-          {
-            // Type of the input,
-            // options: integer | radio | select | color | slider | file | composite | stack
-            type: "integer",
-            name: "The width", // Label for the property
-            property: "width", // CSS property (if buildProps contains it will be extended)
-            units: ["px", "%"], // Units, available only for 'integer' types
-            defaults: "auto", // Default value
-            min: 0, // Min value, available only for 'integer' types
-          },
+        buildProps: [
+          "width",
+          "height",
+          "max-width",
+          "min-height",
+          "margin",
+          "padding",
+        ],
+      },
+      {
+        name: "Typography",
+        open: false,
+        buildProps: [
+          "font-family",
+          "font-size",
+          "font-weight",
+          "letter-spacing",
+          "color",
+          "line-height",
+          "text-shadow",
+        ],
+      },
+      {
+        name: "Decorations",
+        open: false,
+        buildProps: [
+          "border-radius-c",
+          "background-color",
+          "border-radius",
+          "border",
+          "box-shadow",
+          "background",
         ],
       },
       {
         name: "Extra",
         open: false,
-        buildProps: ["background-color", "box-shadow", "custom-prop"],
-        properties: [
-          {
-            id: "custom-prop",
-            name: "Custom Label",
-            property: "font-size",
-            type: "select",
-            defaults: "32px",
-            // List of options, available only for 'select' and 'radio'  types
-            options: [
-              { value: "12px", name: "Tiny" },
-              { value: "18px", name: "Medium" },
-              { value: "32px", name: "Big" },
-            ],
-          },
-        ],
+        buildProps: ["transition", "perspective", "transform"],
+      },
+    ],
+  },
+
+  blockManager: {
+    appendTo: "#blocks",
+    blocks: [
+      {
+        id: "section",
+        label: "<b>Section</b>",
+        attributes: { class: "gjs-block-section" },
+        content: `<section>
+                    <h1>Ceci est un titre simple</h1>
+                    <div>Ceci est juste un texte Lorem : Lorem ipsum dolor sit amet</div>
+                  </section>`,
+      },
+      {
+        id: "text",
+        label: "Texte",
+        content: '<div data-gjs-type="text">Insérez votre texte ici</div>',
+      },
+      {
+        id: "image",
+        label: "Image",
+        select: true,
+        content: { type: "image" },
+        activate: true,
       },
     ],
   },
 });
 
-
-
-// Panel d'export
+// Panels d'export et actions
 editor.Panels.addPanel({
   id: "panel-top",
   el: ".panel__top",
@@ -155,17 +226,17 @@ editor.Panels.addPanel({
   buttons: [
     {
       id: "visibility",
-      active: true, // activé par défaut
+      active: true,
       className: "btn-toggle-borders",
       label: "<u>B</u>",
-      command: "sw-visibility", // Commande intégrée
+      command: "sw-visibility",
     },
     {
       id: "export",
       className: "btn-open-export",
       label: "Exp",
       command: "export-template",
-      context: "export-template", // Pour regrouper le contexte des boutons du même panneau
+      context: "export-template",
     },
     {
       id: "show-json",
@@ -175,9 +246,9 @@ editor.Panels.addPanel({
       command(editor) {
         editor.Modal.setTitle("Composants JSON")
           .setContent(
-            `<textarea style="width:100%; height: 250px;">
-                            ${JSON.stringify(editor.getComponents())}
-                        </textarea>`
+            `<textarea style="width:100%; height: 250px;">${JSON.stringify(
+              editor.getComponents()
+            )}</textarea>`
           )
           .open();
       },
@@ -185,7 +256,7 @@ editor.Panels.addPanel({
   ],
 });
 
-// commandes personnalisées
+// Commandes personnalisées
 editor.Commands.add("show-layers", {
   getRowEl(editor) {
     return editor.getContainer().closest(".editor-row");
@@ -193,14 +264,11 @@ editor.Commands.add("show-layers", {
   getLayersEl(row) {
     return row.querySelector(".layers-container");
   },
-
   run(editor, sender) {
-    const lmEl = this.getLayersEl(this.getRowEl(editor));
-    lmEl.style.display = "";
+    this.getLayersEl(this.getRowEl(editor)).style.display = "";
   },
   stop(editor, sender) {
-    const lmEl = this.getLayersEl(this.getRowEl(editor));
-    lmEl.style.display = "none";
+    this.getLayersEl(this.getRowEl(editor)).style.display = "none";
   },
 });
 editor.Commands.add("show-styles", {
@@ -210,13 +278,21 @@ editor.Commands.add("show-styles", {
   getStyleEl(row) {
     return row.querySelector(".styles-container");
   },
-
   run(editor, sender) {
-    const smEl = this.getStyleEl(this.getRowEl(editor));
-    smEl.style.display = "";
+    this.getStyleEl(this.getRowEl(editor)).style.display = "";
   },
   stop(editor, sender) {
-    const smEl = this.getStyleEl(this.getRowEl(editor));
-    smEl.style.display = "none";
+    this.getStyleEl(this.getRowEl(editor)).style.display = "none";
   },
 });
+editor.Commands.add("set-device-desktop", {
+  run: (editor) => editor.setDevice("Desktop"),
+});
+editor.Commands.add("set-device-tablet", {
+  run: (editor) => editor.setDevice("Tablet"),
+});
+editor.Commands.add("set-device-mobile", {
+  run: (editor) => editor.setDevice("Mobile"),
+});
+
+editor.setDevice("Mobile");
