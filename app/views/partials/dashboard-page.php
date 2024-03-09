4da8398203ea4bee78e33cefeb23c02f0721177c
CHANG-Toma <toma11chang@gmail.com>
2024-03-09 23:03:58 +0100

    feat : Grapesjs set up

--- app/views/partials/dashboard-page.php
+++ app/views/partials/dashboard-page.php
@@ -1,18 +1,3 @@
-<div class="panel__top">
-    <div class="panel__basic-actions"></div>
-    <div class="panel__devices"></div>
-    <div class="panel__switcher"></div>
+<div id="gjs-editor-container">
+    <div id="gjs"></div>
 </div>
-<div class="editor-row">
-    <div class="editor-canvas">
-        <div id="gjs" style="height:0px; overflow:hidden;">
-            <h1>Dashboard</h1>
-        </div>
-    </div>
-    <div class="panel__right">
-        <div class="layers-container"></div>
-        <div class="styles-container"></div>
-    </div>
-</div>
-
-<div id="blocks"></div>
